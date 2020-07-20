b820eebca2bea5074ae484e0f71775dae04f8fa5
Nima Eidipour <nimadeve@gmail.com>
2020-07-20 11:30:58 +0430

    fixed model on repository

--- command/node/repository.js
+++ command/node/repository.js
@@ -6,35 +6,35 @@ import * as ${name.fUC()}Model from '../model/${name}.model';
 
 const  ${name.fUC()}Repository = new (class ${name.fUC()}Repository {
 constructor() {
-  this.${name.fUC()}Model = ${name.fUC()}Model;
+  this.model = ${name.fUC()}Model;
 }
 async index() {
     try {
-      return this.${name.fUC()}Model.find({});
+      return this.model.find({});
     } catch (e) {}
 }
 
 async show(id) {
     try {
-      return this.${name.fUC()}Model.findOne({_id:id});
+      return this.model.findOne({_id:id});
     } catch (e) {}
 }
 
 async insert(data) {
     try {
-      return this.${name.fUC()}Model.create(data);
+      return this.model.create(data);
     } catch (e) {}
 }
 
 async update(find, update) {
     try {
-      return this.${name.fUC()}Model.find(find , update);
+      return this.model.find(find , update);
     } catch (e) {}
 }
 
 async destroy(find) {
     try {
-      return this.${name.fUC()}Model.findOneAndUpdate(find, {
+      return this.model.findOneAndUpdate(find, {
         deletedAt: { date: Date.now() },
       })
     } catch (e) {}
